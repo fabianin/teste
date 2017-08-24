473ebc27c916ea0c7c9bc31581d6f8f53e22ec34
Fabiano Amaral <fabianin.amaral@gmail.com>
2017-08-23 22:31:39 -0300

    adicionado servidor node

--- node-api/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    
-        'secret': 'fabiano amaral freitas',
-        'database': 'postgres://db/'
-    
-    };
--- node-api/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({ 
-    name: String, 
-    password: String, 
-    admin: Boolean 
-});
-module.exports = mongoose.model('User', userSchema);
